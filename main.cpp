f9c1b091e6547cd965dce31b4692f22f23aa7c5a
Tanisharawat14 <tanisharawat818@gmail.com>
2024-07-29 15:44:29 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+    for (int i = 0; i < 1000; i++)
+    {
+        cout << "I love nishthurii!";
+    }
+
+    return 0;
+}
